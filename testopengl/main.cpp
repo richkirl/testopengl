e9bab9a0a12e265dd52ad0bd8176f4be5be7dec9
richkirl <loliiwantlllwnt@gmail.com>
2020-08-24 08:29:33 +0300

    up

--- testopengl/main.cpp
+++ testopengl/main.cpp
@@ -5,19 +5,16 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/matrix_transform_2d.hpp>
 #include <cassert>
 #include <vector>
 #include <sstream>
 #include <fstream>
 using namespace glm;
-
 GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path) {
-
-    // Создаем шейдеры
     GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
     GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-
-    // Загружаем код Вершинного Шейдера из файла
     std::string VertexShaderCode;
     std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
     if (VertexShaderStream.is_open())
@@ -27,8 +24,6 @@ GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path)
         VertexShaderCode = sstr.str();
         VertexShaderStream.close();
     }
-
-    // Загружаем код Фрагментного шейдера из файла
     std::string FragmentShaderCode;
     std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
     if (FragmentShaderStream.is_open()) {
@@ -37,62 +32,46 @@ GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path)
         FragmentShaderCode = sstr.str();
         FragmentShaderStream.close();
     }
-
     GLint Result = GL_FALSE;
     int InfoLogLength;
-
-    // Компилируем Вершинный шейдер
     std::cout << "Компиляция шейдера : " << vertex_file_path << std::endl;
     char const* VertexSourcePointer = VertexShaderCode.c_str();
     glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
     glCompileShader(VertexShaderID);
-
-    // Выполняем проверку Вершинного шейдера
     glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
     if (InfoLogLength > 0) {
-        std::vector<char> VertexShaderErrorMessage((InfoLogLength + 1));
+        std::vector<char> VertexShaderErrorMessage(static_cast<__int64>(InfoLogLength) + 1);
         glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
         std::cout << &VertexShaderErrorMessage[0] << std::endl;
     }
-
-    // Компилируем Фрагментный шейдер
     std::cout << "Компиляция шейдера : " << fragment_file_path << std::endl;
     char const* FragmentSourcePointer = FragmentShaderCode.c_str();
     glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
     glCompileShader(FragmentShaderID);
-
-    // Проверяем Фрагментный шейдер
     glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
     if (InfoLogLength > 0) {
-        std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+        std::vector<char> FragmentShaderErrorMessage(static_cast<__int64>(InfoLogLength) + 1);
         glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
         std::cout << &FragmentShaderErrorMessage[0] << std::endl;
     }
-
-    // Создаем шейдерную программу и привязываем шейдеры к ней
     std::cout << "Создаем шейдерную программу и привязываем шейдеры к ней" << std::endl;
     GLuint ProgramID = glCreateProgram();
     glAttachShader(ProgramID, VertexShaderID);
     glAttachShader(ProgramID, FragmentShaderID);
     glLinkProgram(ProgramID);
-
-    // Проверяем шейдерную программу
     glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
     glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
     if (InfoLogLength > 0) {
-        std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+        std::vector<char> ProgramErrorMessage(static_cast<__int64>(InfoLogLength) + 1);
         glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
         std::cout << &ProgramErrorMessage[0] << std::endl;
     }
-
     glDeleteShader(VertexShaderID);
     glDeleteShader(FragmentShaderID);
-
     return ProgramID;
 }
-
 int main() {
     setlocale(LC_ALL, "Russian");
 	int er = glfwInit();
@@ -102,66 +81,131 @@ int main() {
 	window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
 	assert(window);
 	glfwMakeContextCurrent(window);
-	
-
 	glewExperimental = true;
-    //
     int ui = glewInit();
     assert(!ui);
-	/*if (glewInit() != GLEW_OK) {
-		std::cout << "terminate" << std::endl;
-		return -1;
-	}*/
-
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
-	static const GLfloat g_vertex_buffer_data[] = {
-   -1.0f, -1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-   0.0f,  1.0f, 0.0f,
-	};
-	// Это будет идентификатором нашего буфера вершин
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f,-1.0f,-1.0f, // Треугольник 1 : начало
+       -1.0f,-1.0f, 1.0f,
+       -1.0f, 1.0f, 1.0f, // Треугольник 1 : конец
+     1.0f, 1.0f,-1.0f, // Треугольник 2 : начало
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f, // Треугольник 2 : конец
+        1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f
+    };
+    // Один цвет для каждой вершины
+    static const GLfloat g_color_buffer_data[] = {
+        0.583f,  0.771f,  0.014f,
+        0.609f,  0.115f,  0.436f,
+        0.327f,  0.483f,  0.844f,
+        0.822f,  0.569f,  0.201f,
+        0.435f,  0.602f,  0.223f,
+        0.310f,  0.747f,  0.185f,
+        0.597f,  0.770f,  0.761f,
+        0.559f,  0.436f,  0.730f,
+        0.359f,  0.583f,  0.152f,
+        0.483f,  0.596f,  0.789f,
+        0.559f,  0.861f,  0.639f,
+        0.195f,  0.548f,  0.859f,
+        0.014f,  0.184f,  0.576f,
+        0.771f,  0.328f,  0.970f,
+        0.406f,  0.615f,  0.116f,
+        0.676f,  0.977f,  0.133f,
+        0.971f,  0.572f,  0.833f,
+        0.140f,  0.616f,  0.489f,
+        0.997f,  0.513f,  0.064f,
+        0.945f,  0.719f,  0.592f,
+        0.543f,  0.021f,  0.978f,
+        0.279f,  0.317f,  0.505f,
+        0.167f,  0.620f,  0.077f,
+        0.347f,  0.857f,  0.137f,
+        0.055f,  0.953f,  0.042f,
+        0.714f,  0.505f,  0.345f,
+        0.783f,  0.290f,  0.734f,
+        0.722f,  0.645f,  0.174f,
+        0.302f,  0.455f,  0.848f,
+        0.225f,  0.587f,  0.040f,
+        0.517f,  0.713f,  0.338f,
+        0.053f,  0.959f,  0.120f,
+        0.393f,  0.621f,  0.362f,
+        0.673f,  0.211f,  0.457f,
+        0.820f,  0.883f,  0.371f,
+        0.982f,  0.099f,  0.879f
+    };
 	GLuint vertexbuffer;
-
-	// Создадим 1 буфер и поместим в переменную vertexbuffer его идентификатор
 	glGenBuffers(1, &vertexbuffer);
-
-	// Сделаем только что созданный буфер текущим
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-
-	// Передадим информацию о вершинах в OpenGL
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
     GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
     assert(programID);
+    // Проекционная матрица : 45&deg; поле обзора, 4:3 соотношение сторон, диапазон : 0.1 юнит <-> 100 юнитов
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    // Или, для ортокамеры
+    glm::mat4 View = glm::lookAt(
+        glm::vec3(4, 3, 3), // Камера находится в мировых координатах (4,3,3)
+        glm::vec3(0, 0, 0), // И направлена в начало координат
+        glm::vec3(0, 1, 0)  // "Голова" находится сверху
+    );
+     glm::mat4 Model = glm::mat4(1.0f);  // Индивидуально для каждой модели
+    glm::mat4 MVP = Projection * View * Model; // Помните, что умножение матрицы производиться в обратном порядке
+
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 	do{
         glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glUseProgram(programID);
-
-		// Указываем, что первым буфером атрибутов будут вершины
+        GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // Атрибут 0. Подробнее об этом будет рассказано в части, посвященной шейдерам.
-			3,                  // Размер
-			GL_FLOAT,           // Тип
-			GL_FALSE,           // Указывает, что значения не нормализованы
-			0,                  // Шаг
-			(void*)0            // Смещение массива в буфере
-		);
-
-		// Вывести треугольник!
-		glDrawArrays(GL_TRIANGLES, 0, 3); // Начиная с вершины 0, всего 3 вершины -> один треугольник
-
+		glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+		glDrawArrays(GL_TRIANGLES, 0, 12*3); 
 		glDisableVertexAttribArray(0);
-
+        glDisableVertexAttribArray(1);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
-		
 	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 }
